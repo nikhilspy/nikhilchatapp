32649a560008beb47cccd5686e009e6ef7b245d4
nikhilspy <nikhilchemistry282@gmail.com>
2020-09-11 15:54:26 +0530

    Second commit

--- chat/settings.py
+++ chat/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -130,3 +131,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
